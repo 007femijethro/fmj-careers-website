030c461f3de3cfb97898f2df00c8d02dadf49720
007femijethro <150783278+007femijethro@users.noreply.github.com>
2024-08-23 14:09:09 +0000

    Added the dy pages

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, jsonify
-from database import get_jobs
+from database import get_jobs, get_job
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    jobs = get_jobs()  # Fetch fresh data for each request
+    jobs = get_jobs() 
     return render_template('home.html', jobs=jobs)
 
 @app.route("/jobs")
 def list_jobs():
-    jobs = get_jobs()  # Fetch fresh data for each request
+    jobs = get_jobs()
     return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = get_job(id)  
+    return render_template('jobpage.html', job=job)
+#    if not job:
+#       return "Not Found", 404
+#    return 
+#render_template('jobpage.html', job=job)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -13,16 +13,26 @@ engine = create_engine(
 )
 
 def get_jobs():
-    # Connect to the database and execute a query
     with engine.connect() as conn:
         result = conn.execute(text('SELECT * FROM "fmjJobs"'))
 
-        # Initialize an empty list to store dictionaries
         list_of_jobs = []
 
-        # Convert each row to a dictionary and append it to the list
         for row in result.mappings():
-            row_dict = dict(row)  # Convert row to a dictionary
-            list_of_jobs.append(row_dict)  # Add dictionary to the list
-
+            row_dict = dict(row)  
+            list_of_jobs.append(row_dict) 
     return list_of_jobs
+
+def get_job(id):
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT * FROM "fmjJobs" WHERE id = :val'), {'val': id})
+
+        row = result.mappings().first()  
+    if row is None:
+        return None
+    else:
+        return dict(row) 
+
+
+
+
