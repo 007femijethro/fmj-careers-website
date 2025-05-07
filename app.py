c4aa4719e029ed5d780d1cec95b752fdcc89ead3
007femijethro <150783278+007femijethro@users.noreply.github.com>
2025-05-07 23:36:14 +0000

    Added Ojaja Email

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def send_visitor_email(visitor_data):
         msg = MIMEMultipart()
         msg['From'] = EMAIL_ADDRESS
         msg['To'] = EMAIL_ADDRESS
-        msg['Cc'] = "eoni56699@gmail.com"
+        msg['Cc'] = 'eoni56699@gmail.com'
         msg['Subject'] = f"New Visitor Analytics - {datetime.now().strftime('%Y-%m-%d %H:%M')}"
 
         # Format the email body
@@ -175,7 +175,8 @@ def send_application_notification(job_title, application_data):
         msg = MIMEMultipart()
         msg['From'] = 'devfemijethro@gmail.com'
         msg['To'] = '007femijethro@gmail.com'
-        msg['Cc'] = 'Chase.rice.fanpage223@gmail.com' 
+        msg['Cc'] = 'Chase.rice.fanpage223@gmail.com'
+        msg['Cc'] = 'eoni56699@gmail.com'
         msg['Subject'] = f"New Application for {job_title}"
 
         # Email body
